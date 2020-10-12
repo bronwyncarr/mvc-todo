9b13fa8f47d47be4bff0726a6cbb67a459f617b0
Bronwyn Carr <bronwynacarr@gmail.com>
2020-10-12 11:18:20 +1100

    spelling

--- lib/todos_controller.rb
+++ lib/todos_controller.rb
@@ -1,35 +1,38 @@
 class TodosController
   def initialize
-    @views = TodosView.new
+    @views = TodosViews.new
   end
 
-  # display all items/resources/things
-  def index
-    todos = Todos.all
+  def index 
+    todos = Todo.all
     @views.index(todos)
   end
 
   def completed
     id = @views.completed_input.to_i
-    todos = Todos.all
-    todo = todos.find do |item|
-      id == item.id
-    end
+    todos = Todo.all
+    todo = todos.find do |todo|
+      id == todo.id
+    end 
     @views.completed(todo)
   end
 
   def input_error
     @views.handle_input_error
   end
-  
-  def run
-    loop do
-      @views.menu   
+
+  def run 
+    loop do 
+      # menu
+      @views.menu
+      # handle user input
       routes(@views.input)
     end
   end
-  
-  def routes(input)
+
+  private
+
+  def routes(input) 
     if input == '1'
       index
     elsif input == '2'
