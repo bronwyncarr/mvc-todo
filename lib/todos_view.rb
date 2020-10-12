c4fc5b041e74122a2d1624d6baae2718ad003c17
Bronwyn Carr <bronwynacarr@gmail.com>
2020-10-12 11:22:05 +1100

    todos spelling in views updated

--- lib/todos_view.rb
+++ lib/todos_view.rb
@@ -1,35 +1,34 @@
-class TodosView
-  def index(todos)
-    todos.each do |item|
-      puts "#{item.id}) #{item.name}"
-    end
+class TodosViews
+  def index(todos) 
+    todos.each do |todo|
+      puts "#{todo.id}. #{todo.name}"
+    end 
+  end
+
+  def completed(todo)
+    puts todo.done ? "Completed ✅" : "Not Completed ❌"
   end
 
   def completed_input
-    puts "What todo would you like to check?"
+    puts "What todo would you like to check if its been completed?"
     input
   end
 
-  def completed_input(todo)
-    puts todo.done ? "Completed" : "Not completed"
-  end
-
   def input
-    print "> "
+    print "> " 
     gets.chomp
   end
-
-  def menu
+  
+  def menu 
     puts "Welcome to the todos app!"
     puts "Make your selection"
     puts "1. View all todos"
-    puts "2. Is it done?"
+    puts "2. Is the todo done?"
     puts "3. Exit"
   end
 
-
-
   def handle_input_error
-    puts "you need to enter a 1 or 2."
+    puts "Input error! ❌"
+    puts "You need to input a 1, 2 or 3"
   end
 end
